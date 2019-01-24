8fa8a312bb8ee2742e0d97f1ad5b37e6e4361873
Piera Falcone <pierafalcone01@gmail.com>
2019-01-24 14:08:56 +0100

    First commit

---
+++ main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+void update()
+{
+    printf("Update");
+}
+
+void draw()
+{
+    printf("Draw");
+}
+
+int main()
+{
+    for (;;)
+    {
+        update();
+        draw();
+    }
+
+    return 1;
+}
---
+++ main.h
@@ -0,0 +1,9 @@
+
+
+typedef struct sprite
+{
+    int x;
+    int y;
+    int width;
+    int height;
+} sprite_t;
